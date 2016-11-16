2b0ab00bd20d4457a40ea10e2955c7dccbca7a51
Oleg Shalygin <oshalygin@gmail.com>
2016-11-15 23:25:52 -0800

    Add css loaders

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,29 +1,30 @@
-import path from "path";
-import webpack from "webpack";
+import path from 'path';
+import webpack from 'webpack';
 
 module.exports = {
-  devtool: "eval",
+  devtool: 'eval',
   entry: [
-    "webpack-hot-middleware/client",
-    "./src/index"
+    'webpack-hot-middleware/client',
+    './src/index'
   ],
   output: {
-    path: path.join(__dirname, "dist"),
-    filename: "bundle.js",
-    publicPath: "/static/"
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin()
+
   ],
   module: {
     loaders: [{
       test: /\.jsx?$/,
       exclude: /node_modules/,
-      loaders: ["babel"]
+      loaders: ['babel']
     }, {
       test: /\.jpg/,
-      loader: "file"
+      loader: 'file'
     }]
   }
 };
